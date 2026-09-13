35e228d4a9ab53b355cde200ea31660f87db0b5f
MikeChongZhou <MikeChongZhou@users.noreply.github.com>
2026-09-13 15:44:54 -0400

    Build app as folder distribution

--- build_exe.ps1
+++ build_exe.ps1
@@ -1,7 +1,29 @@
 $ErrorActionPreference = "Stop"
 
-python -m pip install -r requirements.txt
-python -m pip install pyinstaller
-python -m PyInstaller --noconfirm --onefile --windowed --name LocalMeetingRecorder --hidden-import transcriber_worker --hidden-import pycaw --hidden-import pycaw.pycaw --hidden-import pycaw.constants --hidden-import comtypes --hidden-import psutil main.py
+$Python = Join-Path $env:LOCALAPPDATA "Programs\Python\Python313\python.exe"
+if (-not (Test-Path $Python)) {
+    $Python = "python"
+}
 
-Write-Host "Build complete: dist\LocalMeetingRecorder.exe"
+& $Python -m pip install -r requirements.txt
+& $Python -m pip install pyinstaller
+& $Python -m PyInstaller `
+    --noconfirm `
+    --onedir `
+    --windowed `
+    --name LocalMeetingRecorder `
+    --hidden-import transcriber_worker `
+    --hidden-import pycaw `
+    --hidden-import pycaw.pycaw `
+    --hidden-import pycaw.constants `
+    --hidden-import comtypes `
+    --hidden-import psutil `
+    --exclude-module torch `
+    --exclude-module tensorflow `
+    --exclude-module matplotlib `
+    --exclude-module pandas `
+    --exclude-module scipy `
+    --exclude-module sklearn `
+    main.py
+
+Write-Host "Build complete: dist\LocalMeetingRecorder\LocalMeetingRecorder.exe"
